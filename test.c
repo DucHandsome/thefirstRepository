ae5f7d7908e826a4c5d8f3da2cdff16026264736
DucHandsome <duclm.pasal@gmail.com>
2022-09-28 14:48:49 +0700

    Second commit

---
+++ test.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+#include <conio.h>
+
+main()
+
+{
+
+float a,b,c,d,e,f;
+
+printf("n-Nhap so thu nhat = ");
+
+scanf("%f",&a);
+
+printf("n-Nhap so thu hai  = ");
+
+scanf("%f",&b);
+
+printf("n-Nhap so thu ba   = ");
+
+scanf("%f",&c);
+
+printf("n-Nhap so thu tu   = ");
+
+scanf("%f",&d);
+
+e=a+b+c+d;
+
+printf("na * Tong cua 4 so vua nhap vao = %f",e);
+
+f=(a+b+c+d)/4;
+
+printf("n * Trung a binh a cong a cua a 4 a so a la = %f",f);
+
+getch();
+
+}
